1c29145ca69bd54e17c663f1b091b86cb2a26e18
Harsh Kumar Jha <jhaharshkumar293@gmail.com>
2023-08-06 15:36:54 +0530

    Created sidebar header,Search bar and Conversation tab

--- live-chat-client/src/Components/SideBar.jsx
+++ live-chat-client/src/Components/SideBar.jsx
@@ -1,8 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import "../Styles/myStyles.css";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import PersonAddAlt1Icon from "@mui/icons-material/PersonAddAlt1";
+import GroupAddIcon from "@mui/icons-material/GroupAdd";
+import NightlightIcon from "@mui/icons-material/Nightlight";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import { IconButton } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import ConversationItem from "./ConversationItem";
 
 const SideBar = () => {
-  return <div className="sideBar-container">SideBar</div>;
+  const [conversations, setConversation] = useState([
+    {
+      name: "Random1",
+      lastMessage: "Last Message",
+      timeStamp: "14:21:22",
+    },
+    {
+      name: "Random1",
+      lastMessage: "Last Message",
+      timeStamp: "14:21:22",
+    },
+    {
+      name: "Random1",
+      lastMessage: "Testing 123...",
+      timeStamp: "14:21:22",
+    },
+  ]);
+
+  return (
+    <div className="sideBar-container">
+      <div className="sb-header">
+        <div className="">
+          <IconButton>
+            <AccountCircleIcon />
+          </IconButton>
+        </div>
+        <div className="">
+          <IconButton>
+            <PersonAddAlt1Icon />
+          </IconButton>
+          <IconButton>
+            <GroupAddIcon />
+          </IconButton>
+          <IconButton>
+            <AddCircleIcon />
+          </IconButton>
+          <IconButton>
+            <NightlightIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="sb-search">
+        <IconButton>
+          <SearchIcon />
+        </IconButton>
+        <input type="text" className="search-bar" placeholder="Search" />
+      </div>
+      <div className="sb-conversations">
+        {conversations.map((conversation) => {
+          return <ConversationItem props={conversation} />;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default SideBar;
